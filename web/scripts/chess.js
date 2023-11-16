151104037eba9e9e8150f619aede11e6ea3d3d8d
Joshua <joshblue982@gmail.com>
2023-11-17 02:01:28 +1100

    Piece assets
    Created images for all the pieces and loaded them in. Alt text is set to unicode chess characters.

--- web/scripts/chess.js
+++ web/scripts/chess.js
@@ -1,3 +1,13 @@
+
+
+class Piece {
+    constructor(colour, type) {
+        this.colour = colour;
+        this.type = type;
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const chessboardContainer = document.getElementById('chessboard-container');
     const chessboardArray = [
@@ -11,16 +21,29 @@ document.addEventListener('DOMContentLoaded', function () {
         ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
     ];
 
-    const squareColour = ['white', 'black']
+    const charToPNG = {'p':'bP', 'r':'bR', 'n':'bN', 'b':'bB', 'q':'bQ', 'k':'bK',
+                       'P':'wP', 'R':'wR', 'N':'wN', 'B':'wB', 'Q':'wQ', 'K':'wK'};
+
+    const charToUnicode = {'p':'♟︎', 'r':'♜', 'n':'♞', 'b':'♝', 'q':'♛', 'k':'♚',
+                           'P':'♙', 'R':'♖', 'N':'♘', 'B':'♗', 'Q':'♕', 'K':'♔'};
+    
 
     function createChessboard() {
+        const squareColour = ['white', 'black'];
         let colourIndex = 0;
         for (let i = 0; i < 8; i++) {
             for (let j = 0; j < 8; j++) {
                 const square = document.createElement('div');
                 square.classList.add('square');
                 square.classList.add(squareColour[(colourIndex++)%2]);
-                square.textContent = chessboardArray[i][j];
+                let piece = chessboardArray[i][j]; 
+                if (piece in charToPNG) {
+                    const img = document.createElement('img');
+                    img.classList.add('piece');
+                    img.src = '../images/pieces/' + charToPNG[piece] + '.png';
+                    img.alt = charToUnicode[piece];
+                    square.appendChild(img);
+                }
                 chessboardContainer.appendChild(square);
             }
             colourIndex++;
